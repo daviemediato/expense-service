72aa01bc25e5ea0148d8a883a3c76040245d27e7
Yure Moutella <ymoutella@gmail.com>
2025-06-28 16:52:13 -0300

    mqtt implementation

--- src/main/java/com/reimbursement/expense_service/controllers/ExpenseController.java
+++ src/main/java/com/reimbursement/expense_service/controllers/ExpenseController.java
@@ -1,26 +1,37 @@
 package com.reimbursement.expense_service.controllers;
 
-import com.reimbursement.expense_service.dtos.ExpenseDto;
-import com.reimbursement.expense_service.dtos.StatusUpdateDto;
-import com.reimbursement.expense_service.services.ExpenseService;
-import com.reimbursement.expense_service.utils.annotations.Log;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.reimbursement.expense_service.dtos.ExpenseDto;
+import com.reimbursement.expense_service.dtos.StatusUpdateDto;
+import com.reimbursement.expense_service.services.ApprovalTopicService;
+import com.reimbursement.expense_service.services.ExpenseService;
+import com.reimbursement.expense_service.utils.annotations.Log;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "expenses")
-public class ExpenseController
-{
-    private final ExpenseService expenseService;
+public class ExpenseController {
 
-    public ExpenseController(ExpenseService expenseService) {
-        this.expenseService = expenseService;
-    }
+    private final ExpenseService expenseService;
+    private final ApprovalTopicService approvalTopicService;
 
     @Log(message = "Fetching all expenses")
     @GetMapping
@@ -43,13 +54,22 @@ public class ExpenseController
     public ResponseEntity<ExpenseDto> create(@RequestBody @Valid ExpenseDto expenseDto) {
         ExpenseDto createdExpense = this.expenseService.create(expenseDto);
 
+        try {
+
+            StatusUpdateDto approval = new StatusUpdateDto();
+            approval.setExpense_id(createdExpense.getId());
+            approvalTopicService.publishMessage(approval);
+
+        } catch (MqttException e) {
+            System.out.println(e);
+        }
+
         return new ResponseEntity<>(createdExpense, HttpStatus.CREATED);
     }
 
     @Log(message = "Updating an existing expense")
     @PutMapping
-    public ResponseEntity<ExpenseDto> update(@RequestBody @Valid ExpenseDto expenseDto
-    ) {
+    public ResponseEntity<ExpenseDto> update(@RequestBody @Valid ExpenseDto expenseDto) {
         ExpenseDto updatedExpense = this.expenseService.update(expenseDto);
 
         return new ResponseEntity<>(updatedExpense, HttpStatus.OK);
@@ -59,8 +79,7 @@ public class ExpenseController
     @PatchMapping("/{id}/status")
     public ResponseEntity<ExpenseDto> updateStatus(
             @PathVariable UUID id,
-            @RequestBody @Valid StatusUpdateDto statusUpdate
-    ) {
+            @RequestBody @Valid StatusUpdateDto statusUpdate) {
         ExpenseDto updatedExpense = this.expenseService.updateStatus(id, statusUpdate.getStatus());
 
         return new ResponseEntity<>(updatedExpense, HttpStatus.OK);
--- src/main/java/com/reimbursement/expense_service/services/ExpenseService.java
+++ src/main/java/com/reimbursement/expense_service/services/ExpenseService.java
@@ -1,16 +1,23 @@
 package com.reimbursement.expense_service.services;
 
-import com.reimbursement.expense_service.dtos.ExpenseDto;
-
 import java.util.List;
 import java.util.UUID;
 
+import com.reimbursement.expense_service.dtos.ExpenseDto;
+import com.reimbursement.expense_service.enums.Status;
+
 public interface ExpenseService {
     public List<ExpenseDto> findAll();
+
     public ExpenseDto findById(UUID id);
+
     public ExpenseDto create(ExpenseDto expenseDto);
+
     public void softDelete(UUID id);
+
     public void delete(UUID id);
+
     public ExpenseDto update(ExpenseDto expenseDto);
-    public ExpenseDto updateStatus(UUID id, String status);
+
+    public ExpenseDto updateStatus(UUID id, Status status);
 }
